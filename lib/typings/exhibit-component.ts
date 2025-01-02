0a8d359215cae66f179ad14a30b6d06a72079e60
HuangSouth <2168626265@qq.com>
2025-01-02 17:20:34 +0800

    [dev] 添加运行类型

---
+++ lib/typings/exhibit-component.ts
@@ -0,0 +1,27 @@
+export type ExhibitComponent =
+    'INPUT' |
+    'INPUT_NUMBER' |
+    'PASSWORD' |
+    'COLOR' |
+    'SELECT_BY_DICT_VALUE' |
+    'SELECT_BY_DICT_MULTI_VALUE' |
+    'SELECT_BY_DICT_VALUE_SORT_NUMBER' |
+    'SELECT_BY_DICT_HIDE_VALUE_SORT_NUMBER' |
+    'SELECT_BY_DICT_ID' |
+    'SELECT_BY_DICT_MULTI_ID' |
+    'CUSTOM_OPTION_SELECT_BY_DICT_VALUE' |
+    'CUSTOM_OPTION_SELECT_BY_ID' |
+    'CUSTOM_OPTION_SELECT_BY_MULTI_ID' |
+    'CUSTOM_OPTION_SELECT_BY_MULTI_VALUE' |
+    'RATE' |
+    'SLIDER' |
+    'RADIO' |
+    'TEXTAREA' |
+    'TREE_CASCADE' |
+    'CUSTOM_OPTION_TREE_CASCADE' |
+    'CHECKBOX' |
+    'TEXTAREA_JSON' |
+    'ARRAY_BY_TAG'
+
+export type ComponentValueType = 'STRING' | 'NUMBER' | 'BOOLEAN' | 'BASE_ARRAY'
+export type ComponentValue = string | number | boolean | string[] | number[] | boolean[]
---
+++ lib/typings/meta-config.ts
@@ -0,0 +1,53 @@
+import type {ComponentValue, ComponentValueType, ExhibitComponent} from "./exhibit-component.ts";
+
+export type MetaDependencyCondition = 'SOME' | 'NOT_IN' | 'ALL'
+export type OmitDepMetaKeyConfig = Omit<MetaKeyConfig, 'dependencies' | 'masterField'>
+
+export interface MetaConfig {
+    [field: string]: MetaKeyConfig
+}
+
+export interface MetaKeyConfig {
+    id: string
+    title: string
+    field: string
+    display: number
+    readonly: number
+    type: ExhibitComponent
+    required: number
+    options?: MetaOptionConfig[]
+    placeholder: string
+    order: number
+    defaultValue?: ComponentValue
+    valueType?: ComponentValueType
+
+    readonly dependencies?: MetaConfigDependency[]
+}
+
+export interface MetaOptionConfig {
+    name: number
+    id?: string
+    value?: number
+    parentId?: string
+    hideData?: string
+}
+
+export interface MetaConfigDependency {
+    metaKeyMapId: number
+    depField: string
+    depCondition: MetaDependencyCondition
+    depValues: string[]
+    priority: number
+    reset: OmitDepMetaKeyConfig
+}
+
+export interface DataEffect {
+    [masterField: string]: SlaveFieldValueMap[]
+}
+
+export interface SlaveFieldValueMap {
+    slaveField: string
+    slaveValueMap: Record<string, string>[]
+}
+
+
