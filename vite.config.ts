d5cdee2756d407da2d61993998d3bd5afe3ea51f
micolen <2168626265@qq.com>
2025-01-26 19:14:42 +0800

    [dev] 调整打包配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,38 +1,30 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
-import { fileURLToPath, URL } from 'node:url'
-import { resolve } from 'path'
+import {resolve} from 'path'
 
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    plugins: [
+        vue(),
+    ],
+    build: {
+        commonjsOptions: {
+            transformMixedEsModules: true, // 允许混合 CommonJS 和 ESM 模块
+        },
+        lib: {
+            entry: resolve(__dirname, './lib/index.ts'),
+            name: 'g3-config-form',
+            fileName: (format) => `g3-config-form.${format}.js`
+        },
+        cssCodeSplit: true,
+        rollupOptions: {
+            // 确保外部化处理那些你不想打包进库的依赖
+            external: ['vue'],
+            output: {
+                // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
+                globals: {
+                    vue: 'Vue'
+                }
+            }
+        },
     },
-  },
-  build: {
-    commonjsOptions: {
-      transformMixedEsModules: true, // 允许混合 CommonJS 和 ESM 模块
-    },
-    lib: {
-      entry: resolve(__dirname, './lib/index.ts'),
-      name: 'g3-config-form',
-      fileName: (format) => `g3-config-form.${format}.js`
-    },
-    cssCodeSplit: true,
-    rollupOptions: {
-      // 确保外部化处理那些你不想打包进库的依赖
-      external: ['vue'],
-      output: {
-        // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
-        globals: {
-          vue: 'Vue'
-        }
-      }
-    },
-  },
 })
