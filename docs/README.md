40ec4548e7681536c6e6bca4e132ab390c962419
HuangSouth <2168626265@qq.com>
2025-03-25 14:17:17 +0800

    [dev] 调整docs

---
+++ docs/README.md
@@ -0,0 +1,1 @@
+'# Hello guava3shome config form' 
--- lib/typings/exhibit-component.ts
+++
@@ -1,6 +0,0 @@
-export type ComponentValueType =
-    | ((value?: any) => string)   // 对应 String
-    | ((value?: any) => number)   // 对应 Number
-    | ((value?: any) => boolean)  // 对应 Boolean
-    | ((value?: any) => any[])    // 对应 Array
-    | ((value?: any) => object);  // 对应 Object
